e7d59558b3ebb2a79adb04e74cdf2225df258133
Victor Yuri <victoryuritc@yahoo.com>
2023-01-20 15:32:19 -0300

    feat: initial commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import React from "react";
+import "./App.css";
+import rockGlass from "./images/rockGlass.svg";
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+      <span className="logo">TRYBE DELIVERY</span>
+      <object className="rocksGlass" type="image/svg+xml" data={rockGlass}>
         Glass
       </object>
     </div>
