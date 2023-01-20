e7578c94320d5cac2938fd84632cb82c5652979d
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-20 19:44:33 -0300

    build(backend): create product model

---
+++ back-end/src/database/models/Product.js
@@ -0,0 +1,33 @@
+const ProductModel = (sequelize, DataType) => {
+  const Product = sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+      },
+      name: DataType.STRING,
+      price: DataType.DECIMAL,
+      urlImage: DataType.STRING,
+    },
+
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: "products",
+    }
+  );
+  
+  Product.associate = (models) => {
+    Product.hasMany(models.SaleProduct, {
+      as:'product',
+      foreignKey: 'products_id',
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    })
+  }
+
+  return Product;
+};
+
+module.exports = Product;
