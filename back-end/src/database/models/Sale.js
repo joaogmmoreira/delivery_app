507697ae47d02d1a38a33ac517cfe6b715f337c5
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-20 19:55:41 -0300

    build(backend): create sale product model

---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,43 @@
+const SaleModel = (sequelize, DataType) => {
+  const Sale = sequelize.define(
+    "Sale",
+    {
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+      },
+      userId: DataType.INTEGER,
+      sellerId: DataType.INTEGER,
+      totalPrice: DataType.DECIMAL,
+      deliveryAddress: DataType.STRING,
+      deliveryNumber: DataType.STRING,
+      saleDate: DataType.DATE,
+      status: DataType.STRING,
+    },
+
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: "sales",
+    }
+  );
+  
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, {
+      as:'users',
+      foreignKey: 'user_id',
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    })
+    Sale.belongsTo(models.User, {
+      as:'sellers',
+      foreignKey: 'seller_id',
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    })
+  }
+
+  return Sale;
+};
+
+module.exports = Sale;
