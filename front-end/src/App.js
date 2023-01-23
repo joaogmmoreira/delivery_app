72caf351ecdcf2ef513c36131e4115fbafcd7c49
Victor Yuri <victoryuritc@yahoo.com>
2023-01-23 16:33:02 -0300

    feat: redirect user / to /login on App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
+      <Redirect from="/" to="/login" />
       <Switch>
+
         <Route exact path="/login" component={ LoginPage } />
-        <Route exact path="/" component={ HomePage } />
       </Switch>
     </div>
   );
