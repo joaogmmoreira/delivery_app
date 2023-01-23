46bc41e5d92d2c4805d4ca7916c6373b2163014e
Victor Yuri <victoryuritc@yahoo.com>
2023-01-23 20:10:03 -0300

    style: lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const router = require('../routes');
-const cors = require('cors')
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
