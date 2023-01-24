e6135da436a5ab03416e46262117a6874642c6e5
Joao Moreira <jgmoreira1809@gmail.com>
2023-01-24 17:31:06 -0300

    feat: implements products router get method

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const loginRouter = require('./login.router');
+const productsRouter = require('./products.router');
 
 const router = express.Router();
 
 router.use('/login', loginRouter);
+router.use('/products', productsRouter);
 
 module.exports = router;
