a5d4e53a857cdd247a97ab720ac1d0621b64e9ba
Joao Moreira <jgmoreira1809@gmail.com>
2023-01-24 16:36:31 -0300

    feat: '/login/register'

--- back-end/src/services/login.service.js
+++ back-end/src/services/login.service.js
@@ -8,18 +8,38 @@ const login = async ({ email, password }) => {
   const user = await User.findOne({ where: { email } });
 
   if (!user || user.password !== hash) {
-    return { type: 'INVALID_FIELDS', message: 'Invalid fields' };
+    return { type: 404, message: 'Invalid fields' };
   }
 
-  const { dataValues } = user;
-
-  const { password: _, ...payload } = dataValues;
+  const { ...payload } = user.dataValues;
 
   const token = createToken(payload);
   const { id, name, role } = payload;
   return { type: null, message: { name, email, id, role, token } };
 };
 
+const registerUser = async (data) => {
+  const { email, password, name } = data;
+
+  // const verifyUser = await User.findOne({ where: { email } });
+
+  // if (!verifyUser) {
+    const hash = crypto.createHash('md5').update(password).digest('hex');
+
+    const registeredUser = await User.create({ name, email, password: hash, role: 'customer' });
+
+   
+    const { role } = registeredUser.dataValues;
+    
+    const token = createToken(registeredUser.dataValues);
+
+    return { type: null, message: { name, email, role, token } };
+  // }
+
+  // return { type: 400, message: 'User already exists' }
+}
+
 module.exports = {
   login,
+  registerUser,
 };
