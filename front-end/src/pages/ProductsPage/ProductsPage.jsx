6aa8d00d39f0064d56129b48f6ef54d3bba0abfa
Victor Yuri <victoryuritc@yahoo.com>
2023-01-24 19:00:54 -0300

    feat: implement getAllProductsService on ProductsPage

--- front-end/src/pages/ProductsPage/ProductsPage.jsx
+++ front-end/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar';
+import getAllProductsService from '../../services/getAllProductsService';
+import ProductCard from './components/ProductCard';
 
 export default function ProductsPage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function getAllProducts() {
+      const response = await getAllProductsService();
+      setProducts(response.data.message);
+    }
+
+    getAllProducts();
+  }, []);
+
   return (
     <section>
       <Navbar />
       <div>
-        Products Page
+        {
+          products.map((product) => (
+            <ProductCard
+              key={ product.id }
+              product={ product }
+            />))
+        }
       </div>
     </section>
   );
