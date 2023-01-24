ecc99f42f9d1535b90cf7b112396b4ba42bb3afa
Joao Gustavo <jgustavomendonca@gmail.com>
2023-01-24 19:17:35 -0300

    Tela de Registro concluída

--- front-end/src/pages/RegisterPage/RegisterPage.jsx
+++ front-end/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,4 +1,59 @@
+import { useState, useEffect } from 'react';
+
 function RegisterPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [isNameValid, setNameValid] = useState(false);
+  const [isEmailValid, setEmailValid] = useState(false);
+  const [isPasswordValid, setPasswordValid] = useState(false);
+  const [isRegisterBtnDisabled, setIsRegisterBtnDisabled] = useState(true);
+
+  // const [emailExists, setEmailExist] = useState(true);
+
+  const validateEmail = (emailString) => /\S+@\S+\.\S+/.test(emailString);
+
+  useEffect(() => {
+    setNameValid(false);
+
+    const MIN_NAME_LENGTH = 12;
+    if (name.length < MIN_NAME_LENGTH) return;
+
+    setNameValid(true);
+  }, [name]);
+
+  useEffect(() => {
+    setEmailValid(validateEmail(email));
+  }, [email]);
+
+  useEffect(() => {
+    setPasswordValid(false);
+
+    const MIN_PASSWORD_LENGTH = 6;
+
+    if (password.length < MIN_PASSWORD_LENGTH) return;
+
+    setPasswordValid(true);
+  }, [password]);
+
+  useEffect(() => {
+    setIsRegisterBtnDisabled(true);
+
+    if (!isNameValid) return;
+    if (!isEmailValid) return;
+    if (!isPasswordValid) return;
+
+    setIsRegisterBtnDisabled(false);
+  }, [isNameValid, isEmailValid, isPasswordValid]);
+
+  // const handleClickInvalid = () => {
+  //   // setEmailExist(true);
+  //   // if (email !== validateEmail(email)) {
+  //   //   return setEmailExist(false);
+  //   // }
+  // };
+
   return (
     <main>
       <div>Cadastro</div>
@@ -6,23 +61,39 @@ function RegisterPage() {
         type="text"
         placeholder="Seu nome"
         data-testid="common_register__input-name"
+        value={ name }
+        onChange={ ({ target: { value } }) => setName(value) }
       />
       <input
         type="text"
         placeholder="Seu email"
         data-testid="common_register__input-email"
+        value={ email }
+        onChange={ ({ target: { value } }) => setEmail(value) }
       />
       <input
         type="text"
         placeholder="Sua senha"
         data-testid="common_register__input-password"
+        value={ password }
+        onChange={ ({ target: { value } }) => setPassword(value) }
       />
       <button
         type="button"
         data-testid="common_register__button-register"
+        disabled={ isRegisterBtnDisabled }
+        onClick={ () => {} }
       >
         Cadastrar
       </button>
+
+      {/* { !emailExists && (
+        <p
+          data-testid="common_register__element-invalid_register"
+        >
+          This email is invalid!
+        </p>
+      )} */}
       <p
         data-testid="common_register__element-invalid_register"
       >
