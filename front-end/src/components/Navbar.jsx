aecc4749fbfeca88c87973db975b38a9f67295e0
Victor Yuri <victoryuritc@yahoo.com>
2023-01-24 15:58:23 -0300

    feat: add and implement Navbar on dummy ProductsPage

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,9 +1,38 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
+  function handleOnClickLoggout() {
+    localStorage.clear();
+  }
   return (
     <header>
-      <span>
+      <Link
+        to="/customer/producs"
+        data-testid="customer_products__element-navbar-link-products"
+      >
+        PRODUTOS
+      </Link>
+
+      <Link
+        to="/customer/orders"
+        data-testid="customer_products__element-navbar-link-orders"
+      >
+        MEUS PEDIDOS
+      </Link>
+
+      <span
+        data-testid="customer_products__element-navbar-user-full-name"
+      >
         {localStorage.getItem('name')}
       </span>
+
+      <Link
+        onClick={ () => handleOnClickLoggout() }
+        to="/login"
+        data-testid="customer_products__element-navbar-link-logout"
+      >
+        SAIR
+      </Link>
     </header>
   );
 }
---
+++ front-end/src/pages/ProductsPage/ProductsPage.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../../components/Navbar';
+
+export default function ProductsPage() {
+  return (
+    <section>
+      <Navbar />
+      <div>
+        Products Page
+      </div>
+    </section>
+  );
+}
