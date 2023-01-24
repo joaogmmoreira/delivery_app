be6115420a3b6ac8e0154ae2be6e4f9e8a93c227
Victor Yuri <victoryuritc@yahoo.com>
2023-01-24 15:51:58 -0300

    fix: remove Redirect and use history.push on dummy HomePage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
+import ProductsPage from './pages/ProductsPage/ProductsPage';
 
 function App() {
   return (
     <div className="App">
-      <Redirect from="/" to="/login" />
+
       <Switch>
+        <Route exact path="/" component={ HomePage } />
         <Route exact path="/login" component={ LoginPage } />
+        <Route exact path="/customer/products" component={ ProductsPage } />
       </Switch>
     </div>
   );
