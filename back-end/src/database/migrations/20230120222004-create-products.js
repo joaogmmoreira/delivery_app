5c855701ffd7c185b62f2c71371e789e643c44be
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-25 15:13:28 -0300

    fix(db): flot to decimal

--- back-end/src/database/migrations/20230120222004-create-products.js
+++ back-end/src/database/migrations/20230120222004-create-products.js
@@ -14,7 +14,7 @@ module.exports = {
         allowNull: false,
       },
       price: {
-        type: Sequelize.FLOAT,
+        type: Sequelize.DECIMAL(10, 2),
         allowNull: false,
       },
       urlImage: {
