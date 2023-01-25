1db9bede48af0e47593f456fe7c68f287ec3f957
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-25 19:36:01 -0300

    fix(backend): lint

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const loginRouter = require('./login.router');
 const productsRouter = require('./products.router');
 const salesRouter = require('./sales.router');
-const imagesRouter = require('./images.router')
+const imagesRouter = require('./images.router');
 const adminRouter = require('./adimin.router');
 
 const router = express.Router();
@@ -11,6 +11,6 @@ router.use('/login', loginRouter);
 router.use('/products', productsRouter);
 router.use('/sales', salesRouter);
 router.use('/admin', adminRouter);
-router.use('/images', imagesRouter)
+router.use('/images', imagesRouter);
 
 module.exports = router;
