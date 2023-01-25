e20c286b780522d0cf1b346e32a13ffed54603ff
Joao Gustavo <jgustavomendonca@gmail.com>
2023-01-25 18:47:28 -0300

    Começando as validações da tela de Admin

--- front-end/src/pages/AdminPage/AdminPage.jsx
+++ front-end/src/pages/AdminPage/AdminPage.jsx
@@ -1,28 +1,48 @@
+import { useState } from 'react';
+
 function AdminPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [isNameValid, setNameValid] = useState(false);
+  const [isEmailValid, setEmailValid] = useState(false);
+  const [isPasswordValid, setPasswordValid] = useState(false);
+
+  const [adminRegisBtnDisable, setAdminRegisBtnDisable] = useState(true);
+
+  const validateEmail = (emailString) => /\S+@\S+\.\S+/.test(emailString);
+
   return (
     <main>
       <input
         type="text"
         data-testid="admin_manage__input-name"
         placeholder="Nome e sobrenome"
+        // value={}
+        // onChange={}
       />
       <input
         type="email"
         data-testid="admin_manage__input-email"
-        placeholder="seu-email@site.com.br "
+        placeholder="seu-email@site.com.br"
+        // value={}
+        // onChange={}
       />
       <input
         type="password"
         data-testid="admin_manage__input-password"
         placeholder="********"
+        // value={}
+        // onChange={}
       />
-      <select
-        data-testid="admin_manage__select-role"
-      >
-        <option value="Default">Default</option>
-        <option value="Vendedor">Vendedor</option>
-        <option value="Vendedor">Cliente</option>
-      </select>
+      {/* <select
+       data-testid="admin_manage__select-role"
+     >
+       <option value="Default">Default</option>
+       <option value="Vendedor">Vendedor</option>
+       <option value="Vendedor">Cliente</option>
+     </select> */}
       <button
         type="button"
         data-testid="admin_manage__button-register"
