71bd8dfe5b5557450b389fc45d1d9ae7f4f17f11
Victor Yuri <victoryuritc@yahoo.com>
2023-01-26 19:34:17 -0300

    refactor: move components to global scope
    ProductDetailsRow and useProductsRowsMocks

--- front-end/src/components/ProductDetailsRow.jsx
+++ front-end/src/components/ProductDetailsRow.jsx
@@ -1,38 +1,38 @@
 import PropTypes from 'prop-types';
 
-export default function ProductDetailsRow({ product, index, hasRemoveBtn }) {
+export default function ProductDetailsRow({ product, index, hasRemoveBtn, pageName }) {
   console.log(product);
   return (
     <div>
       <span
         data-testid={ `
-        customer_checkout__element-order-table-item-number-${index}` }
+        customer_${pageName}__element-order-table-item-number-${index}` }
       >
         {index + 1}
       </span>
 
       <span
-        data-testid={ `customer_checkout__element-order-table-name-${index}` }
+        data-testid={ `customer_${pageName}__element-order-table-name-${index}` }
       >
         {product.name}
       </span>
 
       <span
-        data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
+        data-testid={ `customer_${pageName}__element-order-table-quantity-${index}` }
       >
         {product.quantity}
       </span>
 
       <span
         data-testid={
-          `customer_checkout__element-order-table-unit-price-${index}`
+          `customer_${pageName}__element-order-table-unit-price-${index}`
         }
       >
         {product.price}
       </span>
 
       <span
-        data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
+        data-testid={ `customer_${pageName}__element-order-table-sub-total-${index}` }
       >
         {product.price}
       </span>
@@ -41,7 +41,7 @@ export default function ProductDetailsRow({ product, index, hasRemoveBtn }) {
           <button
             type="button"
             onClick={ () => {} }
-            data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+            data-testid={ `customer_${pageName}__element-order-table-remove-${index}` }
           >
             Remover
           </button>
@@ -61,4 +61,5 @@ ProductDetailsRow.propTypes = {
   }).isRequired,
   index: PropTypes.number.isRequired,
   hasRemoveBtn: PropTypes.bool.isRequired,
+  pageName: PropTypes.string.isRequired,
 };
