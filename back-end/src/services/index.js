f2d38efe596ac0808d3ba835d18f8accc2150024
Joao Moreira <jgmoreira1809@gmail.com>
2023-01-26 15:41:02 -0300

    implements backend route customer/orders/:id

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -2,10 +2,12 @@ const loginService = require('./login.service');
 const productsService = require('./products.service');
 const salesService = require('./sales.service');
 const adminService = require('./admin.service');
+const customerService = require('./customer.service');
 
 module.exports = {
   loginService,
   productsService,
   salesService,
   adminService,
+  customerService,
 };
