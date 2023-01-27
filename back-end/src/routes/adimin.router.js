87d764b0173084456c265e835c1791a256d05cb3
Silverio Moraes <silverstone191013@gmail.com>
2023-01-27 15:07:01 -0300

    tela de admin backend autrization

--- back-end/src/routes/adimin.router.js
+++ back-end/src/routes/adimin.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { adminController } = require('../controller');
+const { validateAdmin } = require('./validateAdmin');
 
 const router = express.Router();
 
-router.post('/', adminController.registerNewUser);
+router.post('/', validateAdmin, adminController.registerNewUser);
 
 module.exports = router;
