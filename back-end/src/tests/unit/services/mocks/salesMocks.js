fe42dbdc86fb03ca7d906ce0c1c9dc9a5177ae3b
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-30 16:00:46 -0300

    test: :test_tube: salesProducts tests

--- back-end/src/tests/unit/services/mocks/salesMocks.js
+++ back-end/src/tests/unit/services/mocks/salesMocks.js
@@ -25,6 +25,76 @@ const salesModelOutput = [
   },
 ];
 
+const oneSaleModelOutput = {
+  message: {
+    id: 1,
+    userId: 1,
+    sellerId: 1,
+    totalPrice: '13,00',
+    deliveryAddress: 'rua de casa',
+    deliveryNumber: '2',
+    saleDate: '2023-01-25T16:02:39.000Z',
+    status: 'Preparando',
+    user_id: 1,
+    seller_id: 1,
+  },
+};
+
+const productsFromSale = [
+  {
+    saleId: 1,
+    productId: 1,
+    quantity: 2,
+    product_id: 1,
+    sale_id: 1,
+    sales: {
+      id: 1,
+      userId: 1,
+      sellerId: 1,
+      totalPrice: '13',
+      deliveryAddress: 'rua de casa',
+      deliveryNumber: '2',
+      saleDate: '2023-01-25T16:02:39.000Z',
+      status: 'Preparando',
+      user_id: 1,
+      seller_id: 1,
+    },
+    products: {
+      id: 1,
+      name: 'Skol Lata 250ml',
+      price: '2.20',
+      urlImage: 'http://localhost:3001/images/skol_lata_350ml.jpg',
+    },
+  },
+  {
+    saleId: 1,
+    productId: 2,
+    quantity: 4,
+    product_id: 2,
+    sale_id: 1,
+    sales: {
+      id: 1,
+      userId: 1,
+      sellerId: 1,
+      totalPrice: '13',
+      deliveryAddress: 'rua de casa',
+      deliveryNumber: '2',
+      saleDate: '2023-01-25T16:02:39.000Z',
+      status: 'Preparando',
+      user_id: 1,
+      seller_id: 1,
+    },
+    products: {
+      id: 2,
+      name: 'Heineken 600ml',
+      price: '7.50',
+      urlImage: 'http://localhost:3001/images/heineken_600ml.jpg',
+    },
+  },
+];
+
 module.exports = {
   salesModelOutput,
+  oneSaleModelOutput,
+  productsFromSale,
 };
--- back-end/src/tests/unit/services/salesServices.test.js
+++ back-end/src/tests/unit/services/salesServices.test.js
@@ -1,10 +1,10 @@
 const Sinon = require("sinon");
 const { expect } = require("chai");
 
-const { Sale } = require('../../../database/models');
+const { Sale, SaleProduct } = require('../../../database/models');
 const salesService = require('../../../services/sales.service');
 
-const { salesModelOutput } = require("./mocks/salesMocks");
+const { salesModelOutput, oneSaleModelOutput, productsFromSale } = require("./mocks/salesMocks");
 
 describe('Sale Service Tests', () => {
 
@@ -12,7 +12,7 @@ describe('Sale Service Tests', () => {
     Sinon.restore();
   });
 
-  describe('Services', () => {
+  describe('Get sales', () => {
     it('Get All by seller - success', async () => {
       Sinon.stub(Sale, 'findAll').resolves(salesModelOutput);
 
@@ -20,6 +20,39 @@ describe('Sale Service Tests', () => {
 
       expect(result).to.be.deep.equal({type: null, message: salesModelOutput});
     });
+
+    it('Get One sale by id - success', async () => {
+      Sinon.stub(Sale, 'findOne').resolves(oneSaleModelOutput);
+
+      const result = await salesService.getOne(1);
+
+      expect(result).to.be.deep.equal({type: null, message: oneSaleModelOutput});
+    });
+
+    it('Get One sale by id - fail', async () => {
+      Sinon.stub(Sale, 'findOne').resolves(null);
+
+      const result = await salesService.getOne(1);
+
+      expect(result).to.be.deep.equal({ type: 404, message: 'Sale not found' });
+    });
   });
 
+  describe('Get products', () => {
+    it('Get all products from a specific sale - success', async () => {
+      Sinon.stub(SaleProduct, 'findAll').resolves(productsFromSale);
+
+      const result = await salesService.getSaleProducts(1);
+
+      expect(result).to.be.deep.equal({type: null, message: productsFromSale});
+    });
+
+    it('Get all products from a specific sale - fail', async () => {
+      Sinon.stub(SaleProduct, 'findAll').resolves(null);
+
+      const result = await salesService.getSaleProducts(1);
+
+      expect(result).to.be.deep.equal({ type: 404, message: 'No sales registered' });
+    });
+  });
 });
