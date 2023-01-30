be996acac4c3950fa52a5761ff894a1e3d64bb2b
Victor Yuri <victoryuritc@yahoo.com>
2023-01-30 17:00:27 -0300

    feat: implement POST /sales on CheckouPage
    my example

--- front-end/src/pages/CheckoutPage/CheckoutPage.jsx
+++ front-end/src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import CheckoutSellerSelect from './components/CheckoutSellerSelect';
 import ProductDetailsRow from '../../components/ProductDetailsRow';
 /* import useProductsRowsMocks from '../../mocks/useProductsRowsMocks'; */
+import addNewOrder from '../../services/addNewOrder';
 
 export default function CheckoutPage() {
   const [products, setProducts] = useState([]);
-  const [houseNumber, setHouseNumber] = useState('');
-  const [address, setAddress] = useState('');
+  const [deliveryNumber, setDeliveryNumber] = useState('');
+  const [deliveryAddress, setDeliveryAddress] = useState('');
   const [totalPrice, setTotalPrice] = useState(0);
 
   /*   const mockProducts = useProductsRowsMocks(); */
+  const history = useHistory();
 
   function getProductsFromDeliveryCart() {
     const productsFromLocalStorage = JSON
@@ -51,6 +54,34 @@ export default function CheckoutPage() {
     if (products !== 0) { getTotalPrice(); }
   }, [products]);
 
+  async function postNewOrder() {
+    const productToBeAdded = {
+      userId: 1,
+      sellerId: 1,
+      totalPrice: Number(totalPrice.replace(',', '.')),
+      deliveryAddress,
+      deliveryNumber: Number(deliveryNumber),
+      status: 'Preparando',
+      products: [
+        {
+          id: 1,
+          quantity: 3,
+        },
+      ],
+    };
+
+    console.log('Product to be added:', productToBeAdded);
+
+    const response = await addNewOrder(productToBeAdded);
+
+    console.log(response.data.message);
+    history.push(`/customer/orders/${response.data.message.id}`);
+  }
+
+  function handleOnClickSubmitOrder() {
+    postNewOrder();
+  }
+
   return (
     <div>
       <Navbar />
@@ -74,9 +105,7 @@ export default function CheckoutPage() {
       </div>
       <span data-testid="customer_checkout__element-order-total-price">
         {' '}
-        {totalPrice.toLocaleString('pt-br', {
-          style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2,
-        })}
+        {totalPrice }
         {' '}
       </span>
       <div>
@@ -87,16 +116,16 @@ export default function CheckoutPage() {
 
           <input
             data-testid="customer_checkout__input-address"
-            value={ address }
-            onChange={ ({ target: { value } }) => setAddress(value) }
+            value={ deliveryAddress }
+            onChange={ ({ target: { value } }) => setDeliveryAddress(value) }
             type="text"
             placeholder="Travessa Terceira da Castanheira, Bairro Muruci"
           />
 
           <input
             data-testid="customer_checkout__input-address-number"
-            value={ houseNumber }
-            onChange={ ({ target: { value } }) => setHouseNumber(value) }
+            value={ deliveryNumber }
+            onChange={ ({ target: { value } }) => setDeliveryNumber(value) }
             type="text"
             placeholder="198"
           />
@@ -105,7 +134,7 @@ export default function CheckoutPage() {
         <button
           data-testid="customer_checkout__button-submit-order"
           type="button"
-          onClick={ () => {} }
+          onClick={ handleOnClickSubmitOrder }
         >
           Finalizar pedido
         </button>
