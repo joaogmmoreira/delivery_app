6ed6dd842c5b27e1bd432701324b5960c5f7b4e8
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-31 16:21:06 -0300

    feat: :rocket: Implements navbar and button flexibility

---
+++ front-end/src/components/NavbarSeller.jsx
@@ -0,0 +1,36 @@
+import { Link, useHistory } from 'react-router-dom';
+
+export default function NavbarsSeller() {
+  const history = useHistory();
+  function handleOnClickLoggout() {
+    localStorage.clear();
+    history.push('/login');
+  }
+
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return (
+    <header>
+      <Link
+        to="/customer/products"
+        data-testid="customer_products__element-navbar-link-products"
+      >
+        PEDIDOS
+      </Link>
+
+      <span
+        data-testid="customer_products__element-navbar-user-full-name"
+      >
+        {user?.name}
+      </span>
+
+      <button
+        onClick={ handleOnClickLoggout }
+        type="button"
+        data-testid="customer_products__element-navbar-link-logout"
+      >
+        Sair
+      </button>
+    </header>
+  );
+}
